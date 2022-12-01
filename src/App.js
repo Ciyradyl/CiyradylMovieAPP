0159fb75e4c2b0f4e36f144a5a2ab6a33f9c070c
hilmi <kaangenc98@gmail.com>
2022-12-01 14:48:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import MovieBox from "./MovieBox";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Container,
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button
+} from "react-bootstrap";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
+// import { Routes, Route } from "react-router-dom";
+import alertify from "alertifyjs";
+
+const API_POPULAR =
+  "https://api.themoviedb.org/3/movie/popular?api_key=dcbcfe9ab6d2818e853036429ecb24e7";
+const API_TRENDING =
+  "https://api.themoviedb.org/3/trending/all/day?api_key=dcbcfe9ab6d2818e853036429ecb24e7";
+const API_SEARCH =
+  "https://api.themoviedb.org/3/search/movie?api_key=dcbcfe9ab6d2818e853036429ecb24e7&query";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    fetch(API_POPULAR)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  }, []);
+
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    console.log("Searching");
+    try {
+      const url = API_SEARCH + `=${query}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setMovies(data.results);
+      alertify.success("Searched Movies Fetched!", 2);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const searchTrending = async (e) => {
+    e.preventDefault();
+    try {
+      const trending_url = API_TRENDING;
+      const res = await fetch(trending_url);
+      const data = await res.json();
+      console.log(data);
+      setMovies(data.results);
+      alertify.success("Trend Movies Fetched!", 2);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const changeHandler = (e) => {
+    setQuery(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="warning" expand="lg" variant="light">
+        <Container fluid>
+          <Navbar.Brand href="/">MovieDB APP</Navbar.Brand>
+          <Navbar.Brand href="/">Popular</Navbar.Brand>
+          <Navbar.Brand href="/" onClick={searchTrending}>
+            Trending
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
+          <NavbarCollapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-3"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            ></Nav>
+
+            <Form className="d-flex" onSubmit={searchMovie}>
+              <FormControl
+                type="search"
+                placeholder="Movie Name"
+                className="me-2"
+                aria-label="search"
+                name="query"
+                value={query}
+                onChange={changeHandler}
+              ></FormControl>
+              <Button variant="dark" type="submit">
+                Search
+              </Button>
+            </Form>
+          </NavbarCollapse>
+        </Container>
+      </Navbar>
+
+      <div>
+        <div>
+          {movies.length > 0 ?(
+            <div className="container">
+              <div className="grid">
+                {movies.map((movieReq) => (
+                  <MovieBox key={movieReq.id} {...movieReq}></MovieBox>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <h2 className="container">
+              None of the movies in our database match with your search! Please
+              try with another input
+            </h2>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/MovieBox.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import {
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  ModalTitle,
+  Button,
+} from "react-bootstrap";
+
+const API_IMG = "https://image.tmdb.org/t/p/w500/";
+
+const MovieBox = ({
+  title,
+  poster_path,
+  vote_average,
+  release_date,
+  overview,
+}) => {
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
+  return (
+    <div className="card text-center bg-warning m-2">
+      <div className="card-body">
+        <img
+          className="card-img-top"
+          onClick={handleShow}
+          src={API_IMG + poster_path}
+          alt=""
+        ></img>
+
+        <Modal show={show} onHide={handleClose}>
+          <ModalHeader closeButton>
+            <ModalTitle>
+              <h3>{title}</h3>
+            </ModalTitle>
+          </ModalHeader>
+          <ModalBody>
+            <img
+              className="card-img-top"
+              src={API_IMG + poster_path}
+              alt=""
+            ></img>
+          </ModalBody>
+          <ModalBody>
+            <h5>IMDB Rating: {vote_average}</h5>
+            <h5>Release Date: {release_date}</h5>
+            <h7>{overview}</h7>
+          </ModalBody>
+          <ModalFooter>
+            <Button variant="dark" onClick={handleClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    </div>
+  );
+};
+
+export default MovieBox;
